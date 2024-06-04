f84f4fb88f231f6f8dc3c2d38f66ef4e96583e1b
GitHub Action <action@github.com>
2024-06-04 10:18:54 +0000

    Initialize template repository proper
    Set package name to com.github.leegodsrc.velocitymod
    Set project name to VelocityMod
    Set modid to velocitymod

--- src/main/java/com/github/leegodsrc/velocitymod/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/leegodsrc/velocitymod/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.leegodsrc.velocitymod.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
